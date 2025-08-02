1cf834122281766836796190f13d1535a88f385b
Salo (Сало) <62756314+salo2016@users.noreply.github.com>
2025-08-02 21:53:16 +0300

    Shuttle 70 speed

--- Content.Server/Shuttles/Components/ShuttleComponent.cs
+++ Content.Server/Shuttles/Components/ShuttleComponent.cs
@@ -20,9 +20,9 @@ namespace Content.Server.Shuttles.Components
         /// Maximum velocity assuming unupgraded, tier 1 thrusters
         /// </summary>
         [ViewVariables(VVAccess.ReadWrite)]
-        public float BaseMaxLinearVelocity = 20f;
+        public float BaseMaxLinearVelocity = 70f;
 
-        public const float MaxAngularVelocity = 4f;
+        public const float MaxAngularVelocity = 30f;
 
         /// <summary>
         /// The cached thrust available for each cardinal direction
