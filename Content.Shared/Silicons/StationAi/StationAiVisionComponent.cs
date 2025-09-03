34f28e3cf7613b88368e835fc4ddbf5d042cf4b1
HacksLua <parkyrist.ak@gmail.com>
2025-09-03 22:41:17 +0700

    Правки ПР

--- Content.Shared/Silicons/StationAi/StationAiVisionComponent.cs
+++ Content.Shared/Silicons/StationAi/StationAiVisionComponent.cs
@@ -16,5 +16,5 @@ public sealed partial class StationAiVisionComponent : Component
     /// Range in tiles
     /// </summary>
     [DataField, AutoNetworkedField]
-    public float Range = 12.5f; // _Lua
+    public float Range = 12.5f; // _Lua 7.5<12.5 salo fuckup
 }
