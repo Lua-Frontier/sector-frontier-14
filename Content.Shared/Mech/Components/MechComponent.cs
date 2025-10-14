5f0f54d98ad4ddf9fa13be370d041d6e0277b305
HacksLua <parkyrist.ak@gmail.com>
2025-10-14 17:57:50 +0700

    Ребаланс ХП меха

--- Content.Shared/Mech/Components/MechComponent.cs
+++ Content.Shared/Mech/Components/MechComponent.cs
@@ -30,7 +30,7 @@ public sealed partial class MechComponent : Component
     /// The maximum amount of damage the mech can take.
     /// </summary>
     [DataField, AutoNetworkedField, ViewVariables(VVAccess.ReadWrite)]
-    public FixedPoint2 MaxIntegrity = 250;
+    public FixedPoint2 MaxIntegrity = 450;
 
     /// <summary>
     /// How much energy the mech has.
