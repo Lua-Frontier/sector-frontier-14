4750a381b81ec295c0b629a82b6098cbaaaa43ab
Dovacot <dovacot@proton.me>
2025-12-29 19:42:36 +0500

    Ящик сокровищ теперь может хранить части компьютера, зашифрованный диск данных, людскую лопату, диск с РнД очками и пробирку для образцов + перевод лопаты и ящика сокровищ

---
+++ Content.Shared/_Lua/Whitelist/Components/LuaSellableComponent.cs
@@ -0,0 +1,8 @@
+// LuaWorld - This file is licensed under AGPLv3
+// Copyright (c) 2025 LuaWorld
+// See AGPLv3.txt for details.
+
+namespace Content.Shared._Lua.Whitelist.Components;
+
+[RegisterComponent]
+public sealed partial class LuaSellableComponent : Component;
