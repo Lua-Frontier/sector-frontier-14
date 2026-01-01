10c531b5012a58b84d4196b8d77000412fc75ce3
HacksLua <parkyrist.ak@gmail.com>
2026-01-02 02:58:10 +0700

    Ограничение предметов акционеров

--- Content.Shared/_Lua/Shuttles/Components/RadarBlipIconComponent.cs
+++ Content.Shared/_Lua/Shuttles/Components/RadarBlipIconComponent.cs
@@ -1,3 +1,6 @@
+// LuaWorld/LuaCorp - This file is licensed under AGPLv3
+// Copyright (c) 2026 LuaWorld/LuaCorp Contributors
+// See AGPLv3.txt for details.
 using Robust.Shared.GameStates;
 using Robust.Shared.Utility;
 
