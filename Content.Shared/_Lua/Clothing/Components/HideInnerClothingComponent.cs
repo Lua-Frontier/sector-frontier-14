12a766d8f432145fc6f9522c3c65c7d466aefe9e
HacksLua <parkyrist.ak@gmail.com>
2026-01-17 20:03:37 +0700

    Сокрытие jumpsuit

---
+++ Content.Shared/_Lua/Clothing/Components/HideInnerClothingComponent.cs
@@ -0,0 +1,8 @@
+using Robust.Shared.GameStates;
+
+namespace Content.Shared._Lua.Clothing.Components;
+
+[RegisterComponent, NetworkedComponent]
+public sealed partial class HideInnerClothingComponent : Component
+{
+}
