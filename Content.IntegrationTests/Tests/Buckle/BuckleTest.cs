c82bcb81730d47eae8f0b89a990de0619ea260b9
HacksLua <parkyrist.ak@gmail.com>
2026-02-04 14:34:40 +0700

    Revert "Physics Assert in SharedMoverController (#37970)"
    This reverts commit 318f8e8c00aa368e89c91584ab4bd00a4cc68f8f.

--- Content.IntegrationTests/Tests/Buckle/BuckleTest.cs
+++ Content.IntegrationTests/Tests/Buckle/BuckleTest.cs
@@ -32,8 +32,6 @@ namespace Content.IntegrationTests.Tests.Buckle
   - type: Hands
   - type: ComplexInteraction
   - type: InputMover
-  - type: Physics
-    bodyType: KinematicController
   - type: Body
     prototype: Human
   - type: StandingState
