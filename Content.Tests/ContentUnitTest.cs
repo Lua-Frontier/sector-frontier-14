654a43acce506063e63b0415eac40efe21e1ba22
HacksLua <parkyrist.ak@gmail.com>
2026-09-25 22:28:53 +0700

    Не проходит из-за новых зависимостей

--- Content.Tests/ContentUnitTest.cs
+++ Content.Tests/ContentUnitTest.cs
@@ -1,6 +1,20 @@
 using System.Collections.Generic;
 using System.Reflection;
 using Content.Client.IoC;
+using Content.Lua.Common.ChatFilter;
+using Content.Lua.Common.Info;
+using Content.Lua.Common.Networking;
+using Content.Lua.Common.SitePlayerSync;
+using Content.Lua.Common.SponsorPlayer;
+using Content.Lua.Shared.Info;
+using Content.Lua.Server.Administration.UI;
+using Content.Lua.Server.ChatFilter;
+using Content.Lua.Server.Info;
+using Content.Lua.Server.Networking;
+using Content.Lua.Server.Reputation;
+using Content.Lua.Server.SitePlayerSync;
+using Content.Lua.Server.SponsorPlayer;
+using Content.Server.Administration;
 using Content.Server.IoC;
 using Robust.Shared.Analyzers;
 using Robust.Shared.IoC;
@@ -19,12 +33,30 @@ namespace Content.Tests
 
             if (Project == UnitTestProject.Server)
             {
+                RegisterLuaServerIoC(dependencies);
                 ServerContentIoC.Register(dependencies);
             }
             else if (Project == UnitTestProject.Client)
             {
                 ClientContentIoC.Register(dependencies);
             }
+        }
+
+        private static void RegisterLuaServerIoC(IDependencyCollection deps)
+        {
+            deps.Register<ChatFilterManager>();
+            deps.Register<IChatFilterManager, ChatFilterManager>();
+            deps.Register<DecryptFailLogger>();
+            deps.Register<IDecryptFailLogger, DecryptFailLogger>();
+            deps.Register<PublicOfferManager>();
+            deps.Register<IPublicOfferManager, PublicOfferManager>();
+            deps.Register<IPublicOfferGate, PublicOfferManager>();
+            deps.Register<SponsorMusicManager>();
+            deps.Register<ISponsorMusicManager, SponsorMusicManager>();
+            deps.Register<SitePlayerSyncManager>();
+            deps.Register<ISitePlayerSyncManager, SitePlayerSyncManager>();
+            deps.Register<IAlertLevelAdminEuiFactory, AlertLevelAdminEuiFactory>();
+            deps.Register<IReputationModerationEuiFactory, ReputationModerationEuiFactory>();
         }
 
         protected override Assembly[] GetContentAssemblies()
